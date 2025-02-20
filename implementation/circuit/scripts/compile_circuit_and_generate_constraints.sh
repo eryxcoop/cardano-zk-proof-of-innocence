a715c95b5d9add05fb9cbff21915d37fc7366700
jarnesino <julianezequielarnesino@gmail.com>
2025-02-20 16:18:52 -0300

    Add script for generating witnesses with user input and validating them

--- implementation/circuit/scripts/compile_circuit_and_generate_constraints.sh
+++ implementation/circuit/scripts/compile_circuit_and_generate_constraints.sh
@@ -4,10 +4,10 @@ echo "[Circuit compilation](1/4): Compile the circuit"
 circom $CIRCUIT_FILE --r1cs --wasm --sym -p bls12381 -o ${OUTPUT_PATH}
 
 echo "[Circuit compilation](2/4): View information about the circuit"
-snarkjs r1cs info ${OUTPUT_PATH}${CIRCUIT_NAME}.r1cs
+snarkjs r1cs info ${COMPILED_CIRCUIT_CONSTRAINTS_FILE}
 
 echo "[Circuit compilation](3/4): Print the constraints"
-snarkjs r1cs print ${OUTPUT_PATH}${CIRCUIT_NAME}.r1cs ${OUTPUT_PATH}${CIRCUIT_NAME}.sym
+snarkjs r1cs print ${COMPILED_CIRCUIT_CONSTRAINTS_FILE} ${CIRCUIT_OUTPUT_FILES_PREFIX}.sym
 
 echo "[Circuit compilation](4/4): Export r1cs to json"
-snarkjs r1cs export json ${OUTPUT_PATH}${CIRCUIT_NAME}.r1cs ${OUTPUT_PATH}${CIRCUIT_NAME}.r1cs.json
+snarkjs r1cs export json ${COMPILED_CIRCUIT_CONSTRAINTS_FILE} ${COMPILED_CIRCUIT_CONSTRAINTS_FILE}.json
