e5e027a71a3e7838c512f95dc097a4118ddd3798
jarnesino <julianezequielarnesino@gmail.com>
2025-02-20 16:38:36 -0300

    Extract witness file as variable

--- implementation/circuit/scripts/generate_and_validate_witnesses.sh
+++ implementation/circuit/scripts/generate_and_validate_witnesses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[Witness generation](1/2): Generate witness"
-node ${CIRCUIT_OUTPUT_FILES_PREFIX}_js/generate_witness.js ${CIRCUIT_OUTPUT_FILES_PREFIX}_js/${CIRCUIT_NAME}.wasm ${CIRCUIT_INPUT_FILE}  ${OUTPUT_PATH}witness.wtns
+node ${CIRCUIT_OUTPUT_FILES_PREFIX}_js/generate_witness.js ${CIRCUIT_OUTPUT_FILES_PREFIX}_js/${CIRCUIT_NAME}.wasm ${CIRCUIT_INPUT_FILE} ${WITNESS_FILE}
 
 echo "[Witness generation](2/2): Check witness"
-snarkjs wtns check ${COMPILED_CIRCUIT_CONSTRAINTS_FILE}  ${OUTPUT_PATH}witness.wtns
+snarkjs wtns check ${COMPILED_CIRCUIT_CONSTRAINTS_FILE} ${WITNESS_FILE}
