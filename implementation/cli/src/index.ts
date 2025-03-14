b2733a334e83413cb3596336c453e4adfc076447
AgustinBadi <librenotgratis@tuta.io>
2025-03-14 10:22:37 -0300

    Set up the cli program

---
+++ implementation/cli/src/index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import type { BlockfrostProvider, MeshWallet } from '@meshsdk/core';
+
+
+//import { Command } from "commander";
+//import chalk from "chalk";
+//
+//const program = new Command();
+//
+//program
+//  .name("mi-cli")
+//  .description("Una CLI de ejemplo con TypeScript")
+//  .version("1.0.0");
+//
+//program
+//  .command("saludar")
+//  .description("Muestra un saludo personalizado")
+//  .argument("<nombre>", "Nombre de la persona a saludar")
+//  .action((nombre) => {
+//    console.log(chalk.green(`¡Hola, ${nombre}! Bienvenido a mi CLI.`));
+//  });
+//
+//program.parse(process.argv);
