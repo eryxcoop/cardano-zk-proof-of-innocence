6af15c72a852175b5fdce5b0ff7d905f6f103ac4
AgustinBadi <librenotgratis@tuta.io>
2025-03-18 12:42:50 -0300

    Create test to calculate a root hash with one element

--- implementation/cli/src/merkle_tree.test.ts
+++ implementation/cli/src/merkle_tree.test.ts
@@ -1,10 +1,22 @@
 import { expect, test } from 'vitest'
 
+function hashSingleValue(value: number) {
+    return value
+}
+
 class MerkleTree {
+    private list: number[]
+
     constructor(list: number[]) {
         if (list.length == 0) {
             throw new Error("List must not be empty")
         }
+
+        this.list = list
+    }
+
+    root() {
+        return hashSingleValue(this.list[0])
     }
 }
 
@@ -12,5 +24,16 @@ test ("Cannot calculate root hash from an empty list", () => {
     expect (
         () => new MerkleTree([])     
     ).toThrow("List must not be empty")
+})
+
+test ("Can calculate root hash for a list with one element", () => {
+    const merkleTree = new MerkleTree([1])
+
+    const root = merkleTree.root()
+
+    const expectedRoot = hashSingleValue(1)
+
+    expect (root).toEqual(expectedRoot)    
+
 })
 
