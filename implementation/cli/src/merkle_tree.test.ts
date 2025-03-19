5fa172af08418a0905e6250fbadae842355a555a
AgustinBadi <librenotgratis@tuta.io>
2025-03-19 15:23:20 -0300

    Create test to calculate authentication path with 4 elements

--- implementation/cli/src/merkle_tree.test.ts
+++ implementation/cli/src/merkle_tree.test.ts
@@ -33,8 +33,16 @@ class MerkleTree {
         }
     }
 
-    authenticationPathFor(): number[] {
-        return []
+    authenticationPathFor(index: number): number[] {
+        if (this.list.length == 1) {
+            return []
+        } else {
+            if (index < this.halfIndex()) {
+                return this.left().authenticationPathFor(index).concat([this.right().root()])
+            } else {
+                return [this.left().root()].concat(this.right().authenticationPathFor(index))
+            }
+        }
     }
 
     left() {
@@ -99,9 +107,26 @@ test("Cannot create a MKT from an list with size is not pow of 2", () => {
 test("Can calculate authentication path for a list with one element", () => {
     const merkleTree = new MerkleTree([1])
 
-    const authenticationPath = merkleTree.authenticationPathFor([0])
+    const authenticationPath = merkleTree.authenticationPathFor(0)
 
-    const expectedAuthenticationPath = []
+    const expectedAuthenticationPath: number[] = []
 
     expect(authenticationPath).toEqual(expectedAuthenticationPath)
  })
+
+ test("Can calculate authentication path for a list with many elements", () => {
+    const list = [1,2,3,4]
+    const v1 = hashSingleValue(list[0])
+    const v2 = hashSingleValue(list[1])
+    const v3 = hashSingleValue(list[2])
+    const v4 = hashSingleValue(list[3])
+    const v12 = hashPair(v1,v2) 
+    const v34 = hashPair(v3,v4) 
+    const v1234 = hashPair(v12,v34)
+
+    const mkt = new MerkleTree(list)
+    const authenticationPath = mkt.authenticationPathFor(0)
+
+    const expectedAuthenticationPath = [v2,v34]
+    expect(authenticationPath).toEqual(expectedAuthenticationPath)
+})
