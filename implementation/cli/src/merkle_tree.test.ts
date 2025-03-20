62af936eb600eaf0516e5e8ce47361748843fde2
jarnesino <julianezequielarnesino@gmail.com>
2025-03-20 12:38:56 -0300

    Reorganize CLI Merkle tree tests

--- implementation/cli/src/merkle_tree.test.ts
+++ implementation/cli/src/merkle_tree.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 
 type merkleTreeLeafElement = number
 type merkleTreeLeafIndex = number
@@ -71,67 +71,69 @@ class MerkleTree {
     }
 }
 
-test("Cannot create merkle tree from an empty list", () => {
-    expect(
-        () => new MerkleTree([])
-    ).toThrow(MerkleTree.emptyListErrorMessage())
-})
+describe("Merkle tree", () => {
+    test("Cannot create merkle tree from an empty list", () => {
+        expect(
+            () => new MerkleTree([])
+        ).toThrow(MerkleTree.emptyListErrorMessage())
+    })
 
-test("Can calculate root hash for a list with one element", () => {
-    const merkleTree = new MerkleTree([1])
+    test("Cannot create a MKT from an list with size is not pow of 2", () => {
+        expect(
+            () => new MerkleTree([1,2,3,4,5,6])
+        ).toThrow(MerkleTree.listSizeErrorMessage())
+    })
 
-    const root = merkleTree.root()
+    test("Can calculate root hash for a list with one element", () => {
+        const merkleTree = new MerkleTree([1])
 
-    const expectedRoot = hashSingleValue(1)
+        const root = merkleTree.root()
 
-    expect(root).toEqual(expectedRoot)
+        const expectedRoot = hashSingleValue(1)
 
-})
+        expect(root).toEqual(expectedRoot)
 
-test("Can calculate root hash for a list with many elements", () => {
-    const list = [1,2,3,4]
-    const v1 = hashSingleValue(list[0])
-    const v2 = hashSingleValue(list[1])
-    const v3 = hashSingleValue(list[2])
-    const v4 = hashSingleValue(list[3])
-    const v12 = hashPair(v1,v2) 
-    const v34 = hashPair(v3,v4) 
-    const v1234 = hashPair(v12,v34)
+    })
 
-    const mkt = new MerkleTree(list)
-    const root = mkt.root() 
-    expect(root).toEqual(v1234)
-})
+    test("Can calculate root hash for a list with many elements", () => {
+        const list = [1,2,3,4]
+        const v1 = hashSingleValue(list[0])
+        const v2 = hashSingleValue(list[1])
+        const v3 = hashSingleValue(list[2])
+        const v4 = hashSingleValue(list[3])
+        const v12 = hashPair(v1,v2) 
+        const v34 = hashPair(v3,v4) 
+        const v1234 = hashPair(v12,v34)
 
-test("Cannot create a MKT from an list with size is not pow of 2", () => {
-    expect(
-        () => new MerkleTree([1,2,3,4,5,6])
-    ).toThrow(MerkleTree.listSizeErrorMessage())
-})
+        const mkt = new MerkleTree(list)
+        const root = mkt.root() 
+        expect(root).toEqual(v1234)
+    })
 
-test("Can calculate authentication path for a list with one element", () => {
-    const merkleTree = new MerkleTree([1])
+    test("Can calculate authentication path for a list with one element", () => {
+        const merkleTree = new MerkleTree([1])
 
-    const authenticationPath = merkleTree.authenticationPathFor(0)
+        const authenticationPath = merkleTree.authenticationPathFor(0)
 
-    const expectedAuthenticationPath: hash[] = []
+        const expectedAuthenticationPath: hash[] = []
 
-    expect(authenticationPath).toEqual(expectedAuthenticationPath)
- })
+        expect(authenticationPath).toEqual(expectedAuthenticationPath)
+    })
 
- test("Can calculate authentication path for a list with many elements", () => {
-    const list = [1,2,3,4]
-    const v1 = hashSingleValue(list[0])
-    const v2 = hashSingleValue(list[1])
-    const v3 = hashSingleValue(list[2])
-    const v4 = hashSingleValue(list[3])
-    const v12 = hashPair(v1,v2) 
-    const v34 = hashPair(v3,v4) 
-    const v1234 = hashPair(v12,v34)
+    test("Can calculate authentication path for a list with many elements", () => {
+        const list = [1,2,3,4]
+        const v1 = hashSingleValue(list[0])
+        const v2 = hashSingleValue(list[1])
+        const v3 = hashSingleValue(list[2])
+        const v4 = hashSingleValue(list[3])
+        const v12 = hashPair(v1,v2) 
+        const v34 = hashPair(v3,v4) 
+        const v1234 = hashPair(v12,v34)
 
-    const mkt = new MerkleTree(list)
-    const authenticationPath = mkt.authenticationPathFor(0)
+        const mkt = new MerkleTree(list)
+        const authenticationPath = mkt.authenticationPathFor(0)
 
-    const expectedAuthenticationPath = [v2,v34]
-    expect(authenticationPath).toEqual(expectedAuthenticationPath)
+        const expectedAuthenticationPath = [v2,v34]
+        expect(authenticationPath).toEqual(expectedAuthenticationPath)
+    })
 })
